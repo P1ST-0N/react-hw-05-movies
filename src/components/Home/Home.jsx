8aff361debf952db41ef451893ff13cfd18544db
P1ST-0N <luckylionia@gmail.com>
2023-05-26 18:33:59 +0300

    deploy site on gh

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,42 +1,42 @@
-import React, { useState, useEffect } from 'react';
-// import { Link, useLocation } from 'react-router-dom';
-// import { ListMovies, MovieItem } from './home.styled';
-import { getTrendingMovies } from '../../shared/services/api';
-import css from './home.module.css';
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { ListMovies, MovieItem } from "./home.styled";
+import { getTrendingMovies } from "../../shared/services/api";
+import css from "./home.module.css";
 
 import { HomeList } from "../HomeList/HomeList";
 
 const Home = () => {
-  // const [data, setData] = useState(null);
-  // const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  // const location = useLocation();
+  const location = useLocation();
 
-  const [films, setFilms] = useState([]);
+  // const [films, setFilms] = useState([]);
+  // useEffect(() => {
+  //   getTrendingMovies().then((data) => setFilms(data.results));
+  // }, []);
+
   useEffect(() => {
-    getTrendingMovies().then((data) => setFilms(data.results));
+    const getData = async () => {
+      try {
+        setLoading(true);
+        const data = await getTrendingMovies();
+        setData(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
   }, []);
-
-// useEffect(() => {
-//   const getData = async () => {
-//     try {
-//       setLoading(true);
-//       const data = await getTrendingMovies();
-//       setData(data);
-//       setLoading(false);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   getData();
-// }, []);
 
   return (
     <>
-      <h1 className={css.title}>Trending today</h1>
-      <HomeList films={films} />
+      {/* <h1 className={css.title}>Trending today</h1>
+      <HomeList films={films} /> */}
 
-      {/* <ListMovies>
+      <ListMovies>
         <h2 className={css.title}>Trending movies:</h2>
         {loading
           ? "Loading..."
@@ -51,7 +51,7 @@ const Home = () => {
                 </Link>
               </MovieItem>
             ))}
-      </ListMovies> */}
+      </ListMovies>
     </>
   );
 };
